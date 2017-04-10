10a13e4782fbd03d36b6025d3fb4c4e1b585cede
MappX <quickmarcher@outlook.com>
2017-04-10 23:02:20 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://mappx.github.io/leaflet-maps-with-google-sheets/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_0WrX_kD9zPQN5NtGq4TvvVekOmWLekaDFYQe_w9o6s/edit#gid=0';
